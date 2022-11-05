53b46b01cc89a42b519eda9a26228d79d7190f83
Pedrael <mykhailo.kryvytskyi@right.link>
2022-11-05 03:31:04 +0200

    added provider state management

---
+++ lib/widgets/dropdown.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HebrewBearDropdown extends StatefulWidget {
+  const HebrewBearDropdown({super.key});
+
+  @override
+  State<HebrewBearDropdown> createState() => _HebrewBearDropdownState();
+}
+
+class _HebrewBearDropdownState extends State<HebrewBearDropdown> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
